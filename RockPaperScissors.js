3eaf54832de8fd6b9e2ab286f15d57a423c9151d
iBePixel <53214529+iBePixel@users.noreply.github.com>
2024-07-23 16:56:52 +0100

    Update RockPaperScissors.js

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,19 +1,28 @@
-let num
-let CGuess 
+let num;
+let CGuess;
+let UGuess;
 
-function CPUGuess() {
-    CGuess = ''
-    num = Math.random()
-    if (num >= 0.66){
-        CGuess= 'Rock';
-    }
-    else if (num >= 0.33){
-        CGuess= 'Paper';
-    }
-    else {
-        CGuess= 'Scissors';
-    }
-    return CGuess 
+function getComputerChoice() {
+  CGuess = "";
+  num = Math.random();
+  if (num >= 0.66) {
+    CGuess = "Rock";
+  } else if (num >= 0.33) {
+    CGuess = "Paper";
+  } else {
+    CGuess = "Scissors";
+  }
+  return CGuess;
 }
 
-console.log(CPUGuess())
+function getUserChoice() {
+  let UChoice = prompt("Enter your choice. Rock, Paper or Scissors");
+  if ((UChoice != "Rock") | "Paper" | "Scissors") {
+    return "please enter Rock, Paper or Scissors";
+    
+  }
+  return UChoice
+}
+
+console.log(getComputerChoice());
+console.log(getUserChoice());
