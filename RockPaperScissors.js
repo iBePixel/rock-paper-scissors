cfae9dd582409fca66e7e4caf1c2c9be0227ddb2
iBePixel <53214529+iBePixel@users.noreply.github.com>
2024-07-24 14:57:44 +0100

    initial Rock Paper Scissors project complete.

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -2,27 +2,62 @@ let num;
 let CGuess;
 let UGuess;
 
+//invoke score variables for CPU and User.
+let CScore = 0;
+let UScore = 0;
+
+
 function getComputerChoice() {
   CGuess = "";
   num = Math.random();
   if (num >= 0.66) {
-    CGuess = "Rock";
+    CGuess = "rock";
   } else if (num >= 0.33) {
-    CGuess = "Paper";
+    CGuess = "paper";
   } else {
-    CGuess = "Scissors";
+    CGuess = "scissors";
   }
   return CGuess;
 }
 
 function getUserChoice() {
   let UChoice = prompt("Enter your choice. Rock, Paper or Scissors");
-  if ((UChoice != "Rock") | "Paper" | "Scissors") {
-    return "please enter Rock, Paper or Scissors";
-    
-  }
+  UChoice = UChoice.toLowerCase()
   return UChoice
 }
 
-console.log(getComputerChoice());
-console.log(getUserChoice());
+function playRound(userChoice, computerChoice) {
+  
+  if (userChoice == computerChoice){
+    console.log("It's a draw, try again")
+    return "draw";
+  }
+  else if (userChoice == 'rock' && computerChoice == 'scissors' || userChoice == 'scissors' && computerChoice == 'paper' || userChoice == 'paper' && computerChoice == 'rock'){
+    UScore++;
+    console.log("You win " + userChoice + " beats " + computerChoice)
+  }
+  else {
+    CScore++;
+    console.log("You lose " + computerChoice + " beats " + userChoice)
+  }
+}
+
+
+function playGame(){
+  for (let i = 0; i < 5; i++) {
+    let userSelection = getUserChoice();
+    let computerSelection = getComputerChoice();
+    let result = playRound(userSelection, computerSelection);
+    if (result == "draw"){
+      i = i - 1;
+    }
+  }
+  if (UScore > CScore) {
+    console.log("Congratulations, you win!")
+  }
+  else {
+    console.log("Bad luck, you lost. Better luck next time.")
+  }
+}
+
+playGame()
